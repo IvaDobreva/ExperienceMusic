d1d0efc9858a0540d906d61052aaa8d089eca3fd
Iva Dobreva <iva95dobreva@gmail.com>
2017-05-29 22:52:15 +0900

    moving from the old repo

---
+++ scripts/formValidation.js
@@ -0,0 +1,11 @@
+function validate() {
+  var usrName = document.login.Name.value;
+  var pass = document.login.password.value;
+
+
+  console.log(usrName.length);
+  //Validate userName
+  if(usrName.length) {
+
+  }
+}
