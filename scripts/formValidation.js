8ce0def18b0fe8fd8d9940d212e61fd0dcac672d
Iva Dobreva <iva95dobreva@gmail.com>
2017-05-30 02:15:43 +0900

    Registration form validation done

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,11 +1,28 @@
 function validate() {
-  var usrName = document.login.Name.value;
-  var pass = document.login.password.value;
+  var usrName = document.registration.Name.value;
+  var pass = document.registration.password.value;
+  var phone = document.registration.Phone.value;
 
+  var usrRegEx = /([A-Z0-9]+)([a-z_]*)/;
+  var passRegEx = /([a-z]{3,})|([0-9]{3,})|([A-Z]{3,})/;
+  var phoneRegEx = /([0-9]{3})([-])([0-9]{4})(-)([0-9]{4})/;
 
-  console.log(usrName.length);
-  //Validate userName
-  if(usrName.length) {
+  var usrError = "Username length must be between 6-10, containing only English letters, numbers, and \"_\"!";
+  var passError = "Password length must be between 6-10, containing letters, numbers, and special characters<br>There cannot be more than 3 characters from the same set!";
+  var phoneError = "Please, enter your phone number correctly!";
 
+  if(!usrName.match(usrRegEx) || (usrName.length<6 || usrName.length>10 )){
+    document.getElementById('usrError').innerHTML = usrError;
+    event.preventDefault();
+  }
+
+  if(pass.length<6 || pass.length>10 || pass.match(passRegEx)) {
+    document.getElementById('passError').innerHTML = passError;
+    event.preventDefault();
+  }
+
+  if((phone.length!=13) || (!phone.match(phoneRegEx))) {
+    document.getElementById('phoneError').innerHTML = phoneError;
+    event.preventDefault();
   }
 }
