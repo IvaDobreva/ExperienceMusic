af9e7f096e17a5384aa6aaac4d2716ec9b0a7384
Iva Dobreva <iva95dobreva@gmail.com>
2017-05-30 02:22:28 +0900

    Comments on validation script

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,12 +1,16 @@
 function validate() {
+
+  //Getting information from the form
   var usrName = document.registration.Name.value;
   var pass = document.registration.password.value;
   var phone = document.registration.Phone.value;
 
+  //Regular Expressions
   var usrRegEx = /([A-Z0-9]+)([a-z_]*)/;
   var passRegEx = /([a-z]{3,})|([0-9]{3,})|([A-Z]{3,})/;
   var phoneRegEx = /([0-9]{3})([-])([0-9]{4})(-)([0-9]{4})/;
 
+  //Error Strings
   var usrError = "Username length must be between 6-10, containing only English letters, numbers, and \"_\"!";
   var passError = "Password length must be between 6-10, containing letters, numbers, and special characters<br>There cannot be more than 3 characters from the same set!";
   var phoneError = "Please, enter your phone number correctly!";
