e993f9af2ba7a8e617b4ebb72e0c360430671426
Iva Dobreva <iva95dobreva@gmail.com>
2017-06-07 15:58:57 +0900

    renamed file

--- register.php
+++ register.php
@@ -1,15 +1,16 @@
 <?php
 
-require_once('api.php');
+require_once('dbConnection.php');
 
 $username = ($_POST['Name']);
 $passw = ($_POST['password']);
 $birthdate = ($_POST['BirthDate']);
 $phone = ($_POST['Phone']);
 $address = ($_POST['Address']);
+$loginNum = 0;
 
-$sql = "INSERT INTO user (username, password, birthdate, phone, address)
-VALUES ($username, $passw, $birthdate, $phone, $address)";
+$sql = "INSERT INTO user (username, password, birthdate, phone, address, loginNum)
+VALUES ($username, $passw, $birthdate, $phone, $address, $loginNum)";
 
 $conn->query($sql);
  ?>
